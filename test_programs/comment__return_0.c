af741bda40d527e2bfc3a56fcc4107242309c3b4
Harvard2018 <36239927+Harvard2018@users.noreply.github.com>
2018-03-07 22:45:42 +0800

    Update comment__return_0.c

--- test_programs/comment__return_0.c
+++ test_programs/comment__return_0.c
@@ -1,5 +1,42 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+# Here's the file we want to compile.
+$ cat return_two.c
+#include <stdio.h>
+ 
 int main() {
-    /* hello world
-       return 1; */
-    return 0;
+    return 2;
 }
+ 
+# Run the compiler with this file.
+$ ./babyc return_two.c
+Written out.s.
+ 
+# Check the output looks sensible.
+$ cat out.s
+.text
+    .global _start
+ 
+_start:
+    movl    $2, %ebx
+    movl    $1, %eax
+    int     $0x80
