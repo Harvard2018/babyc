23d3dd8374c2ccf38dac60532662ed59ba85b03e
Harvard2018 <36239927+Harvard2018@users.noreply.github.com>
2018-03-07 22:44:18 +0800

    Update addition__return_3.c

--- test_programs/addition__return_3.c
+++ test_programs/addition__return_3.c
@@ -1,1 +1,9 @@
-int main() { return 1 + 2; }
+# Assemble the file. We explicitly assemble as 32-bit
+# to avoid confusion on x86_64 machines.
+$ as out.s -o out.o --32
+ 
+# Link the file, again specifying 32-bit.
+$ ld -m elf_i386 -s -o out out.o
+ 
+# Run it!
+$ ./out
